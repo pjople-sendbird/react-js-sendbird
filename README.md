73ea4bc348e3a37cdc629e72919cb323fdb623a5
Walter Rodriguez (Sendbird) <walter.rodriguez@sendbird.com>
2021-12-02 09:40:23 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# SendBird Chat Client Overview
+Chat client built as a learning project. Current state allows users to enter the app by entering a username. Users can select or create a channel from the /channels screen. Within the /chat screen, one to many users will be able to send/ receive messages, load previous messages, view active participants, leave the channel, and delete the channel if they are an admin/ creator of the channel.
+
+There are three main screens within the app: 
+- Login
+- Channels
+- Chat 
+
+## Tech Used
+- React
+- [SendBird](https://www.sendbird.com)
+- Material UI (and CSS-in-JS)
+- dotenv (SendBird API key)
+- React Router
+- Redux/ React Redux
---
+++ src/components/screens/Login/Login.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import LoginButton from "./LoginButton/LoginButton";
+import styles from "./Login.styles";
+import PropTypes from "prop-types";
+import logo from "./sbchat_logo.png";
+
+class Login extends React.Component {
+	state = {
+		username: ""
+	};
+
+	handleChange = event => {
+		this.setState({ username: event.target.value });
+	};
+
+	render() {
+		const {
+			classes: { loginContainer },
+			history
+		} = this.props;
+		return (
+			<div className={loginContainer}>
+				<img src={logo} alt="logo" />
+				<TextField
+					id="username"
+					placeholder="Enter a username"
+					margin="normal"
+					label="Username"
+					InputLabelProps={{ shrink: true }}
+					value={this.state.username}
+					onChange={this.handleChange}
+				/>
+				<LoginButton
+					id="loginButton"
+					setSB={this.props.setSB}
+					history={history}
+					username={this.state.username}
+				/>
+			</div>
+		);
+	}
+}
+
+Login.propTypes = {
+	history: PropTypes.object.isRequired,
+	setSB: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(Login);
